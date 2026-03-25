d7ca9797d307a7f45a7c95b04ee6fc68e4b62704
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-03-25 20:19:57 +0300

    update: add fullwidth mode

--- client/src/shared/ui/input/Input.tsx
+++ client/src/shared/ui/input/Input.tsx
@@ -3,17 +3,20 @@ import type { InputHTMLAttributes } from 'react';
 import styles from './Input.module.css';
 
 type Props = InputHTMLAttributes<HTMLInputElement> & {
+  variant?: "primary" | "secondary";
   label?: string;
+  fullWidth?: boolean;
   error?: string;
   warning?: boolean;
   onClear?: () => void;
+
 };
 
-export function Input({ label, error, warning, onClear, className = '', ...props }: Props) {
+export function Input({ label, error, warning, onClear, className = '', variant='secondary', fullWidth=false,  ...props }: Props) {
   return (
-    <label className={styles.wrapper}>
-      {label ? <span className={styles.label}>{label}</span> : null}
-      <span className={`${styles.field} ${warning ? styles.warning : ''} ${error ? styles.error : ''}`}>
+    <label className={`${styles.wrapper} ${fullWidth ? styles.fullWidth : ''}`}>
+      {label ? <span className={variant==='primary' ? styles.labelStrong : styles.label}>{label}</span> : null}
+      <span className={`${styles.field}  ${warning ? styles.warning : ''} ${error ? styles.error : ''}`}>
         <input className={`${styles.input} ${className}`} {...props} />
         {onClear && props.value ? (
           <button className={styles.clear} type="button" onClick={onClear} aria-label="Очистить поле">
